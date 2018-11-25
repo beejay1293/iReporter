bc90c4f7394d8986c56a9f7f0c389a64a655e984
Mobolaji <mattimobolaji@gmail.com>
2018-11-25 17:14:38 +0100

    now we can get a specific red flag from our data

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,12 @@
 const incident = [{
-  redflags: [{
-    id1: {
-      id: 1, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'a new incident',
+  redflags: [
+    {
+      id: 0, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'a new incident',
     },
-    id2: {
-      id: 2, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'another incident',
+    {
+      id: 1, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'another incident',
     },
-  }],
+  ],
   interventions: [{}],
 }];
 
@@ -15,7 +15,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/redflags', (req, res, next) => {
-  res.status(200).json({ status: 200, data: incident[0].redflags });
+  const data = incident[0].redflags;
+  res.status(200).json({ status: 200, data });
+});
+
+router.get('/redflags/:id', (req, res, next) => {
+  const data = incident[0].redflags[req.params.id];
+
+  res.status(200).json({ status: 200, data });
 });
 
 
