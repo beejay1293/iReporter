6e6cdc29ae0214fb5d357619f7836c50499614cf
Mobolaji <mattimobolaji@gmail.com>
2018-11-27 16:05:50 +0100

    delete specific flag

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,3 @@
-
 const incident = [{
   redflags: [
     {
@@ -16,37 +15,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
-
-router.get('/redflags', (req, res, next) => {
-  const data = incident[0].redflags;
-  res.status(200).json({ status: 200, data });
-});
-
-router.get('/redflags/:id', (req, res, next) => {
-  const data = incident[0].redflags[req.params.id];
-
-  res.status(200).json({ status: 200, data: [data] });
-});
-
-router.post('/redflags',  (req, res, next) => {
-  incident[0].redflags.push(req.body);
-  res.status(200).json({ status: 200, data: [{ id: req.body.id, message: 'Created red-flag record' }] });
-});
-
-router.put('/redflags/:id/location', (req, res, next) => {
-  const data = incident[0].redflags[req.params.id];
-  data.location = req.body.location;
-  res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records location' }] });
-
-})
-
-router.put('/redflags/:id/comment', (req, res, next) => {
-  const data = incident[0].redflags[req.params.id];
-  data.comment = req.body.comment;
-  res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records comment' }] });
-
-})
 
 router.delete('/redflags/:id', (req, res, next) => {
   const data = incident[0].redflags
--- test/test.js
+++ test/test.js
@@ -1,9 +1,22 @@
-const assert = require('assert');
+const assert = require('assert')
+const chai = require("chai");
+const chaiHttp = require('chai-http');
+const app = require('../index');
+const should = chai.should();
 
-describe('Array', function() {
-  describe('#indexOf()', function()  {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+chai.use(chaiHttp);
+
+describe('/DELETE redflag by id', () => {
+  it('it should delete a redflag record', (done) => {
+    chai.request(app)
+      .delete('/api/v1/redflags/:id')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.be.a('array')
+        res.body.data[0].should.be.a('object')
+        res.body.data[0].message.should.be.eql('red-flag record has been deleted')
+        res.body.data.length.should.be.eql(1);
+        done();
+      })
+  })
+})
