092075b7a2b2fb7bf9cd6b77aaa2f4dcf58bb593
Mobolaji <mattimobolaji@gmail.com>
2018-11-28 15:25:01 +0100

    edit red flag

--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
-const assert = require('assert');
+const assert = require('assert')
+const chai = require("chai");
+const chaiHttp = require('chai-http');
+const app = require('../index');
+const should = chai.should();
 
-describe('Array', function() {
-  describe('#indexOf()', function()  {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+chai.use(chaiHttp);
+
+
+describe('/PUT redflag', () => {
+  it('it should edit a specific redflag record comment', (done) => {
+    chai.request(app)
+      .put('/api/v1/redflags/0/comment')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.be.a('array')
+        res.body.data[0].should.be.a('object')
+        res.body.data[0].message.should.be.eql('Updated red-flag records comment')
+        res.body.data.length.should.be.eql(1);
+
+        done();
+
+      })
+  })
+})
