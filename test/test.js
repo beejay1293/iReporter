f0cd9ffd75f7dfb726cf525dee3e537fe7ca75fb
Mobolaji <mattimobolaji@gmail.com>
2018-11-29 19:14:12 +0100

    new commit

--- test/test.js
+++ test/test.js
@@ -6,3 +6,17 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+
+describe('/GET redflag by id', () => {
+  it('it should get redflag by id', (done) => {
+    chai.request(app)
+      .get('/api/v1/redflags/:id')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.be.a('array')
+        res.body.data.length.should.be.eql(1);
+        done();
+        process.exit(0);
+      })
+  })
+})
