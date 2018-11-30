b4de227d84345c2cf155b7889cede98eab4bc4b4
Mobolaji <mattimobolaji@gmail.com>
2018-11-30 17:02:16 +0100

    new commit

--- test/test.js
+++ test/test.js
@@ -79,3 +79,19 @@ describe('/PUT redflag', () => {
       })
   })
 })
+
+describe('/DELETE redflag by id', () => {
+  it('it should delete a redflag record', (done) => {
+    chai.request(app)
+      .delete('/api/v1/redflags/:id')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.data.should.be.a('array')
+        res.body.data[0].should.be.a('object')
+        res.body.data[0].message.should.be.eql('red-flag record has been deleted')
+        res.body.data.length.should.be.eql(1);
+        done();
+        process.exit(0);
+      });
+  });
+})
