2ce0a6aaea25dd11c1ed4b10bc75f68b3f9f55a3
Mobolaji <mattimobolaji@gmail.com>
2018-11-30 14:31:49 +0100

    new commit

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+
 const incident = [{
   redflags: [
     {
@@ -10,16 +11,22 @@ const incident = [{
   interventions: [{}],
 }];
 
-
 const express = require('express');
 
 const router = express.Router();
 
 
+
+
+
+
 router.get('/redflags/:id', (req, res, next) => {
   const data = incident[0].redflags[req.params.id];
+
   res.status(200).json({ status: 200, data: [data] });
 });
+
+
 
 
 
