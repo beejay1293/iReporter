ee401f0ff91378d487dc89e5a2749f9ea9fc8468
Mobolaji <mattimobolaji@gmail.com>
2018-11-30 15:06:16 +0100

    new commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const routes = require('./routes/api');
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
 
-
 app.use((req, res, next) => {
   const error = new Error('Not found');
   error.status = 404;
