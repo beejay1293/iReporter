b4043c7abaf86aa8441a2ee395f1ed1dad612c50
Mobolaji <mattimobolaji@gmail.com>
2018-12-05 23:41:29 +0100

    develop

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,10 @@
 const incident = [{
   redflags: [
     {
-      id: 12, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'a new incident',
+      id: 0, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'a new incident',
     },
     {
-      id: 10, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'another incident',
+      id: 1, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'another incident',
     },
   ],
   interventions: [{}],
@@ -18,6 +18,35 @@ const router = express.Router();
 
 
 
+router.get('/redflags', (req, res, next) => {
+  const data = incident[0].redflags;
+  res.status(200).json({ status: 200, data });
+});
+
+router.get('/redflags/:id', (req, res, next) => {
+  const data = incident[0].redflags[req.params.id];
+
+  res.status(200).json({ status: 200, data: [data] });
+});
+
+router.post('/redflags',  (req, res, next) => {
+  incident[0].redflags.push(req.body);
+  res.status(200).json({ status: 200, data: [{ id: req.body.id, message: 'Created red-flag record' }] });
+});
+
+router.put('/redflags/:id/location', (req, res, next) => {
+  const data = incident[0].redflags[req.params.id];
+  data.location = req.body.location;
+  res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records location' }] });
+
+})
+
+router.put('/redflags/:id/comment', (req, res, next) => {
+  const data = incident[0].redflags[req.params.id];
+  data.comment = req.body.comment;
+  res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records comment' }] });
+
+})
 
 router.delete('/redflags/:id', (req, res, next) => {
   const data = incident[0].redflags
@@ -25,8 +54,6 @@ router.delete('/redflags/:id', (req, res, next) => {
 
   res.status(200).json({ status: 200, data: [{id: req.params.id, message: 'red-flag record has been deleted' }] })
 })
-
-
 
 
 
