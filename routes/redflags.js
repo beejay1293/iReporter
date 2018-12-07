ab203ea628954ae554e3e63ec5a96b7489f705b1
Mobolaji <mattimobolaji@gmail.com>
2018-12-07 12:24:34 +0100

    feat(UI): Worked on my UI based on my LFA feedback -https://github.com/beejay1293/iReporter -https://www.pivotaltracker.com/n/projects/2226578  -A full stack application that enables citizens to bring any form of co rruption to the notice of appropriate authorities  [Delivers #162051431]

--- routes/redflags.js
+++ routes/redflags.js
@@ -1,21 +1,8 @@
-
-const incident = [{
-  redflags: [
-    {
-      id: 0, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'a new incident',
-    },
-    {
-      id: 1, createdOn: new Date(), createdBy: 1, type: 'redflags', location: 'location', status: 'draft', images: [], videos: [], comment: 'another incident',
-    },
-  ],
-  interventions: [{}],
-}];
-
 const express = require('express');
 
 const router = express.Router();
 
-
+const incident = require('../controller/incident.js');
 
 
 router.get('/redflags', (req, res, next) => {
@@ -29,7 +16,7 @@ router.get('/redflags/:id', (req, res, next) => {
   res.status(200).json({ status: 200, data: [data] });
 });
 
-router.post('/redflags',  (req, res, next) => {
+router.post('/redflags', (req, res, next) => {
   incident[0].redflags.push(req.body);
   res.status(200).json({ status: 200, data: [{ id: req.body.id, message: 'Created red-flag record' }] });
 });
@@ -38,21 +25,19 @@ router.put('/redflags/:id/location', (req, res, next) => {
   const data = incident[0].redflags[req.params.id];
   data.location = req.body.location;
   res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records location' }] });
-
-})
+});
 
 router.put('/redflags/:id/comment', (req, res, next) => {
   const data = incident[0].redflags[req.params.id];
   data.comment = req.body.comment;
   res.status(200).json({ status: 200, data: [{ id: req.params.id, message: 'Updated red-flag records comment' }] });
-
-})
+});
 
 router.delete('/redflags/:id', (req, res, next) => {
-  const data = incident[0].redflags
-  const removed = data.splice(req.params.id, 1)
+  const data = incident[0].redflags;
+  const removed = data.splice(req.params.id, 1);
 
-  res.status(200).json({ status: 200, data: [{id: req.params.id, message: 'red-flag record has been deleted' }] })
+  res.status(200).json({ status: 200, data:[ {id: req.params.id, message: 'red-flag record has been deleted' }] })
 })
 
 
